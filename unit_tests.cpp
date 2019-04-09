b64cebd63a0837a99e6d2db7b6c75467331dbdf3
Raul Costa Junior <raulcostajunior@gmail.com>
2019-04-09 22:41:56 +0200

    Changed the existing tests to reflect the temporary public availability of readHeader.

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -6,9 +6,11 @@
 
 TEST_CASE("File Header", "Header") {
     KeePassFile kpFile("../libkeepass/db_samples/kp_1.kdbx"); // Password is "kppass"
+    kpFile.readHeader();
     REQUIRE(kpFile.formatVersion() == FormatVersion::KDBX_2);
     REQUIRE((kpFile.fileVersion().major == 3 && kpFile.fileVersion().minor == 1));
     REQUIRE(kpFile.isPayloadCompressed() == true);
     REQUIRE(kpFile.masterSeed().size() == 32);
     REQUIRE(kpFile.headerSize() == 222);
+    REQUIRE(kpFile.innerRandStreamId() == InnerStreamEncryption::SALSA20);
 }
